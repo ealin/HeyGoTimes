e58160959c9a0e199ca35078a579ab7716e4fcc9
Ealin@Mac <ealin.chiu@gmail.com>
2011-05-03 18:07:01 +0800

    report page (haed and tail only)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,10 +21,16 @@ module ApplicationHelper
   #-----------------------------------------------------------------------------------
   # method: show_page_head      (Ealin: 20110502)
   #   - 包含以下聯結: to main page/ to today's newspaper / logout
+  #     editor_flag = true ==>  多顯示一個'editor' link
   #-----------------------------------------------------------------------------------
   # 
-  def show_page_head
+  def show_page_head (editor_flag )
     
+    if editor_flag == true
+      @edit_flag = true
+    else
+      @edit_flag = false
+    end
     render  '/layouts/show_page_head'
   end
   #===========================================================================
