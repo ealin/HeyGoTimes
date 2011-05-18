8d77d769b2e1d3f43034777a9d4b8d8f79eab86d
Charlie Chen <peijiong@gmail.com>
2011-05-18 19:01:10 +0800

    update paper layout

--- app/controllers/paper_controller.rb
+++ app/controllers/paper_controller.rb
@@ -1,26 +1,32 @@
-class PaperController < NewsController
+class PaperController < ApplicationController
 
   def index
-     check_followed()   #一開始就要判斷目前user是否有訂閱這份報紙
-     
-     # for checking login status in View, we must define an obj-attribute @logged_flag and
-     #    check_logged_in() in application_controller.rb
-     check_logged_in()
-     
-     set_newspaper_size()
-     get_paper_title_info()
-   end
+    @news = get_news()
 
+    check_followed()   #一開始就要判斷目前user是否有訂閱這份報紙
+
+    # for checking login status in View, we must define an obj-attribute @logged_flag and
+    #    check_logged_in() in application_controller.rb
+    check_logged_in()
+
+    set_newspaper_size()
+    get_paper_title_info()
+  end
+
+  def get_news
+    @news = News.all
+    return @news
+  end
 
   #-----------------------------------------------------------------------------------
   # method: set_newspaper_size      (Ealin: 20110501)
   #   - （設定報紙頁面中, 一些與size有關的attributes）
   #-----------------------------------------------------------------------------------
-  # 
+  #
   def set_newspaper_size
-    @paper_width = 600 
+    @paper_width = 600
     @title_height = 210
-    
+
     @ad_width = 120
     @ad_height = 60
     @ad_no = 10
@@ -75,7 +81,7 @@ class PaperController < NewsController
   #-----------------------------------------------------------------------------------
   # 
   def show_paper_content
-    @news = News.all
+
      # render _show_paper_content.html.erb
      render :layout => nil
    
