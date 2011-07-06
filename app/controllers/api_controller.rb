f604b1d189a7640ea458e7ccbd84aa7bf7acf04f
Ealin@Mac <ealin.chiu@gmail.com>
2011-07-07 01:36:18 +0800

    robot - duplicate problem

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,11 +15,15 @@ class ApiController < ApplicationController
       # Check URL existence
       @news = News.find_by_url(params[:url])
       if (@news != nil)
-          @data[:ret] = 'url exist'
+          response_str = 'duplicate!!'
+          respond_to do |format|
+            format.html { render  :inline => response_str }
+          end
+
+          return
       end
 
       # Parse data
-      if (@data[:ret] != 'url exist')
 
         # @url = 'http://www.facebook.com/sharer.php?u=' + params[:url]
         # @url = 'http://developers.facebook.com/tools/lint/?url=' + URI.encode(params[:url])
@@ -67,49 +71,45 @@ class ApiController < ApplicationController
         @data[:title]=title.to_s
         @data[:image]=image_url.to_s
         @data[:text]=text.to_s
-      end
 
-      #
-      # create a new news record and save to db  (reference to news_contrller/create
-      #
-      @news = News.new(params[:news])
-      @user = User.find(2)   #reporter
-      @news.user = @user
+          #
+          # create a new news record and save to db  (reference to news_contrller/create
+          #
+          @news = News.new(params[:news])
+          @user = User.find(2)   #reporter
+          @news.user = @user
 
-      @news.title = @data[:title]
-      @news.content = @data[:text]
-      @news.url = params[:url]
+          @news.title = @data[:title]
+          @news.content = @data[:text]
+          @news.url = params[:url]
+          @news.special_flag= true
 
-      @image = Image.create(@data[:image])
-      @image.news = @news
-      @image.save
+          @image = Image.create(@data[:image])
+          @image.news = @news
+          @image.save
 
-      @news.tags = []
-      tags = Tag.all
-      tags.each do |tag|
-        if params[:tags].include?(tag.name)
-          @news.tags << tag  # many-to-many relationship ==> it would be saved to DB automatically
-        end
-      end
+          @news.tags = []
+          tags = Tag.all
+          tags.each do |tag|
+            if params[:tags].include?(tag.name)
+              @news.tags << tag  # many-to-many relationship ==> it would be saved to DB automatically
+            end
+          end
 
-      # setup areas of this news
-      areas = Area.all
-      @news.areas = []
-      areas.each do |area|
-        if params[:area].include?(area.name)
-          @news.areas << area  # many-to-many relationship ==> it would be saved to DB automatically
-        end
-      end
+          # setup areas of this news
+          areas = Area.all
+          @news.areas = []
+          areas.each do |area|
+            if params[:area].include?(area.name)
+              @news.areas << area  # many-to-many relationship ==> it would be saved to DB automatically
+            end
+          end
 
-      @news.save
+          @news.save
     end
 
 
-    if @data[:ret] == 'url exist'
-      response_str = 'duplicate!!'
-    else
-      response_str = "title=" + @data[:title] + "  text=" + @data[:text] + "  image url="+  @data[:image]
-    end
+    response_str = "title=" + @data[:title] + "  text=" + @data[:text] + "  image url="+  @data[:image]
 
     respond_to do |format|
       format.html { render  :inline => response_str }
