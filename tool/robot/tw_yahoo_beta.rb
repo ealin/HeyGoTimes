3b7d0390182f29d643fccca67bb91efc8ccdd747
Ealin@Mac <ealin.chiu@gmail.com>
2011-07-12 01:25:27 +0800

    yahoo-beta Robot

--- tool/robot/tw_yahoo_beta.rb
+++ tool/robot/tw_yahoo_beta.rb
@@ -4,30 +4,115 @@ require 'net/http'
 
     yahoo_rss_url = [
       "http://beta.tw.news.yahoo.com/rss/entertainment",
-      "",
- ] ;
+      "http://beta.tw.news.yahoo.com/rss/celebrity",
+      "http://beta.tw.news.yahoo.com/rss/music",
+      "http://beta.tw.news.yahoo.com/rss/tv-radio",
+      "http://beta.tw.news.yahoo.com/rss/movies",
+      "http://beta.tw.news.yahoo.com/rss/jp-kr",
+
+      "http://beta.tw.news.yahoo.com/rss/sports",
+      "http://beta.tw.news.yahoo.com/rss/basketball",
+      "http://beta.tw.news.yahoo.com/rss/baseball",
+      "http://beta.tw.news.yahoo.com/rss/golf",
+      "http://beta.tw.news.yahoo.com/rss/tennis",
+      "http://beta.tw.news.yahoo.com/rss/other-sports",
+
+      "http://beta.tw.news.yahoo.com/rss/politics",
+
+      "http://beta.tw.news.yahoo.com/rss/society",
+
+      "http://beta.tw.news.yahoo.com/rss/world",
+      "http://beta.tw.news.yahoo.com/rss/asia-australia",
+      "http://beta.tw.news.yahoo.com/rss/china",
+      "http://beta.tw.news.yahoo.com/rss/america",
+      "http://beta.tw.news.yahoo.com/rss/euro-africa",
+
+      "http://beta.tw.news.yahoo.com/rss/finance",
+      "http://beta.tw.news.yahoo.com/rss/stock",
+      "http://beta.tw.news.yahoo.com/rss/industry",
+      "http://beta.tw.news.yahoo.com/rss/economy",
+      "http://beta.tw.news.yahoo.com/rss/real-estate",
+      "http://beta.tw.news.yahoo.com/rss/money-career",
+
+      "http://beta.tw.news.yahoo.com/rss/local/",
+      "http://beta.tw.news.yahoo.com/rss/taipei",
+      "http://beta.tw.news.yahoo.com/rss/north-taiwan",
+      "http://beta.tw.news.yahoo.com/rss/mid-taiwan",
+      "http://beta.tw.news.yahoo.com/rss/south-taiwan",
+      "http://beta.tw.news.yahoo.com/rss/east-taiwan",
+
+      "http://beta.tw.news.yahoo.com/rss/lifestyle",
+      "http://beta.tw.news.yahoo.com/rss/weather",
+      "http://beta.tw.news.yahoo.com/rss/consumption",
+      "http://beta.tw.news.yahoo.com/rss/travel",
+      "http://beta.tw.news.yahoo.com/rss/education",
+      "http://beta.tw.news.yahoo.com/rss/art",
+      "http://beta.tw.news.yahoo.com/rss/lifestyle",
+
+      "http://beta.tw.news.yahoo.com/rss/health",
+      "http://beta.tw.news.yahoo.com/rss/disease",
+      "http://beta.tw.news.yahoo.com/rss/beauty",
+
+      "http://beta.tw.news.yahoo.com/rss/technology",
+      "http://beta.tw.news.yahoo.com/rss/information-3c",
+      "http://beta.tw.news.yahoo.com/rss/science" ] ;
 
 
     yahoo_rss_tag = [
 
+        "Entertainment","Entertainment","Entertainment","Entertainment","Entertainment","Entertainment",
 
-
-
+        "Sport",
+        "Sport",
+        "Sport",
+        "Sport",
+        "Sport",
+        "Sport",
 
         "Politics",
+
         "Society",
-        "Local",
+
         "World",
+        "World",
+        "World",
+        "World",
+        "World",
+
         "Business",
-        "Sci_Tech",
-        "Sport",
-        "Health",
+        "Business",
+        "Business",
+        "Business",
+        "Business",
+        "Business",
+
+        "Local",
+        "Local",
+        "Local",
+        "Local",
+        "Local",
+        "Local",
+
+        "Life",
+        "Life",
+        "Life",
+        "Travel",
         "Education",
         "Art",
-        "Entertainment",
-        "Travel",
         "Life",
-        "Special", "Special","Special","Special","Special","Special","Special","Special"] ;
+
+        "Health",
+        "Health",
+        "Health",
+
+        "Sci_Tech",
+        "Sci_Tech",
+        "Sci_Tech"] ;
+
+
+    #yahoo_rss_area = [
+     #   ] ;
+
 
 
     link = ""
@@ -50,8 +135,7 @@ while true
             open(feed_url) do |feed|
               RSS::Parser.parse(feed.read , false).items.each do |item|
 
-                index = (item.link).index("*")
-                link = (item.link)[(index+1)..(item.link).length]
+                link = URI.encode(item.link)
 
                 puts 'News Link :' + link
 
