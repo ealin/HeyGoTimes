daa23456e299acb9de0f4f8f105e7f18661d42f9
Charlie Chen <peijiong@gmail.com>
2011-07-13 11:41:06 +0800

    udpate var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -117,17 +117,17 @@ class ApplicationController < ActionController::Base
   #   - # get user friends and save to friendship table
   #-----------------------------------------------------------------------------------
   def get_friends(id)
-    @facebook_cookies = Koala::Facebook::OAuth.new(Facebooker2.app_id, Facebooker2.secret).get_user_info_from_cookie(cookies)
-    @access_token = @facebook_cookies["access_token"]
-    @graph = Koala::Facebook::GraphAPI.new(@access_token)
-    @friends = @graph.get_connections("me", "friends")
+    facebook_cookies = Koala::Facebook::OAuth.new(Facebooker2.app_id, Facebooker2.secret).get_user_info_from_cookie(cookies)
+    access_token = facebook_cookies["access_token"]
+    graph = Koala::Facebook::GraphAPI.new(access_token)
+    friends = graph.get_connections("me", "friends")
 
-    @user = User.find(id)
+    user = User.find(id)
 
-    @friends.each do |friend|
-      @friend = User.find_by_host_id(friend["id"])
-      if (@friend != nil && !@user.friends.include?(@friend))
-        @user.friends << @friend
+    friends.each do |friend|
+      friend = User.find_by_host_id(friend["id"])
+      if (friend != nil && !user.friends.include?(friend))
+        user.friends << friend
       end
     end
   end
