7ce75b4977c8e43740d0d0e8ffc44d33a4e4e306
Charlie Chen <peijiong@gmail.com>
2011-07-15 16:15:46 +0800

    update loading animate

--- db/schema.rb
+++ db/schema.rb
@@ -128,12 +128,14 @@ ActiveRecord::Schema.define(:version => 20110629040839) do
     t.string   "locale"
     t.datetime "birthday"
     t.string   "host_account"
-    t.integer  "host_id"
+    t.integer  "host_id",             :limit => 8
     t.integer  "host_site"
-    t.boolean  "admin",               :default => false
+    t.boolean  "admin",                            :default => false
     t.date     "last_update_friends"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["host_id"], :name => "index_users_on_host_id"
 
 end
