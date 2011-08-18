8cfd8df11ca20285bbe805fff26c380465e002d6
Ealin@Mac <ealin.chiu@gmail.com>
2011-08-18 13:34:54 +0800

    skip load-more +15,+20,+25

--- app/controllers/paper_controller.rb
+++ app/controllers/paper_controller.rb
@@ -76,7 +76,7 @@ class PaperController < NewsController
     end
 
     if temp_str == nil || (temp_str != '10'&& temp_str != '15' && temp_str != '20' && temp_str != '25')
-      @loading_news_num = 10
+      @loading_news_num = 15
     else
       @loading_news_num = Integer(temp_str)
     end
@@ -104,7 +104,7 @@ class PaperController < NewsController
   #
   def get_news(type, page)
     if @loading_news_num == nil || (@loading_news_num != 10 && @loading_news_num != 15 && @loading_news_num != 20 && @loading_news_num != 25)
-      @loading_news_num = 10
+      @loading_news_num = 15
     end
 
     user_areas = []
@@ -173,7 +173,7 @@ class PaperController < NewsController
 
   def get_special_news(type, page)
     if @loading_news_num == nil || (@loading_news_num != 10 && @loading_news_num != 15 && @loading_news_num != 20 && @loading_news_num != 25)
-      @loading_news_num = 10
+      @loading_news_num = 15
     end
 
     user_id = nil
