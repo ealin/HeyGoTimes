2ad3ca4f6cb7ea7028ae150c2a3f7dc5be542bf1
Ealin@Mac <ealin.chiu@gmail.com>
2011-09-23 17:39:53 +0800

    apple news parser

--- app/controllers/parser/parser_main.rb
+++ app/controllers/parser/parser_main.rb
@@ -3,16 +3,22 @@ def news_parser(url)
   require 'nokogiri'
 
   require 'parser/tw_yahoo_parser.rb'
+  require 'parser/tw_apple_parser.rb'
   require 'parser/fb_debugger_parser.rb'
 
   @parser_data = Hash.new
 
   if url.include? 'tw.news.yahoo'
     type = :tw_yahoo
+  elsif (url.include? 'rss.feedsportal.com') || (url.include? 'tw.nextmedia.com')
+    type = :tw_apple
   end
+
 
   if type == :tw_yahoo
     parse_tw_yahoo(url)
+  elsif type == :tw_apple
+    parse_tw_apple(url)
   else
     parse_fb_debugger(url)
   end
